f1d955a77a871761d559a0c1f19fe29d6c775a33
rushilbhat1 <bhatrushil@outlook.com>
2023-12-08 14:11:01 +0000

    Implement solution

--- employee.py
+++ employee.py
@@ -5,11 +5,60 @@ class Employee:
     def __init__(self, name):
         self.name = name
 
-    def get_pay(self):
-        pass
+        if name == 'Billie':
+            self.salary = 4000
+        elif name == 'Charlie':
+            self.hourly_wage = 25
+            self.hours_worked = 100
+        elif name == 'Renee':
+            self.salary = 3000
+            self.contracts_landed = 4
+            self.commission_per_contract = 200
+        elif name == 'Jan':
+            self.hourly_wage = 25
+            self.hours_worked = 150
+            self.contracts_landed = 3
+            self.commission_per_contract = 220
+        elif name == 'Robbie':
+            self.salary = 2000
+            self.bonus = 1500
+        elif name == 'Ariel':
+            self.hourly_wage = 30
+            self.hours_worked = 120
+            self.bonus = 600
+        
+
+    def get_pay(self):  
+        total_pay = 0
+        if hasattr(self, 'salary'):
+            total_pay += self.salary
+        else:
+            total_pay += self.hourly_wage * self.hours_worked
+
+
+        if hasattr(self, 'bonus'):
+            total_pay += self.bonus
+        elif hasattr(self, 'contracts_landed'):
+            total_pay += self.contracts_landed * self.commission_per_contract
+
+        return total_pay
+
 
     def __str__(self):
-        return self.name
+        description = f"{self.name} works on a "
+        if hasattr(self, 'salary'):
+            description += f"monthly salary of {self.salary}"
+        else:
+            description += f"contract of {self.hours_worked} hours at {self.hourly_wage}/hour"
+
+        if hasattr(self, 'bonus'):
+            description += f" and receives a bonus commission of {self.bonus}"
+        elif hasattr(self, 'contracts_landed'):
+            description += f" and receives a commission for {self.contracts_landed} contract(s) at {self.commission_per_contract}/contract"
+
+        description += f". Their total pay is {self.get_pay()}."    
+        return description
+
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
